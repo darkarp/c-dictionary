cf2133593b87140e76f6e4c89e9596b8df41196c
Mario Nascimento <mario@whitehathacking.tech>
2022-06-28 18:21:07 +0200

    Fix overflow

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Observation
 - Still in development. Run `make` for a list of self-explanatory options.
-- If you want to try `hashtest.py`, you have to run `python3 setup.py install` to build and install the C extension used by `hashtest.py`
-    - Note that you **must** change the first include in `calculations.c` for the module installation to succeed. Open the file and read the first comment, it explains the situation.
+- If you want to try `collisions.py`, you have to run `python3 setup.py install` to build and install the C extension used by `collisions.py`
+    - Note that you **must** make sure the `Python.h` include in `calculations.c` for the module installation to succeed. On Linux all you have to do is `sudo apt install python3-dev`. On Windows, you must find the `Python.h` file in the installation directory and change the include manually to point to it.
 
+## TODO
+    * Documentation
+    * Utility functions
+    * Stability check
+    * Multiple hash support
+    * File & membuffer I/O for saving/loading
--- calculations.c
+++ calculations.c
@@ -1,66 +1,68 @@
 //! Include Python.h from your python installation dir
-#include "C:/Users/darkarp/AppData/Local/Programs/Python/Python310/include/Python.h"
 #include "dict/headers/dict.h"
+#include <Python.h>
 #include <stdlib.h>
 #include <time.h>
 
-static PyObject* method_factors(PyObject* self, PyObject* args) {
-    int number = 0;
+typedef unsigned long long luint;
 
-    // parse argument "number", k= unsigned long long without overflow check
-    if (!PyArg_ParseTuple(args, "k", &number)) {
-        return NULL;
-    }
+static PyObject *method_factors(PyObject *self, PyObject *args) {
+  luint number = 0;
 
-    PyObject* list = PyList_New(0);
-    if (number % 2 == 0) {
-        PyList_Append(list, Py_BuildValue("i", 2));
-        number /= 2;
-        while (number % 2 == 0) number /= 2;
+  // parse argument "number", K= unsigned long long
+  if (!PyArg_ParseTuple(args, "K", &number)) {
+    return NULL;
+  }
+
+  PyObject *list = PyList_New(0);
+  if (number % 2 == 0) {
+    PyList_Append(list, Py_BuildValue("i", 2));
+    number /= 2;
+    while (number % 2 == 0)
+      number /= 2;
+  }
+  for (int i = 3; i <= sqrt(number); i = i + 2) {
+    while (number % i == 0) {
+      number /= i;
+      PyList_Append(list, Py_BuildValue("i", i));
     }
-    for (int i = 3; i <= sqrt(number); i = i + 2) {
-        while (number % i == 0) {
-            number /= i;
-            PyList_Append(list, Py_BuildValue("i", i));
-        }
-    }
-    if (number > 2) PyList_Append(list, Py_BuildValue("i", number));
-    return list;
+  }
+  if (number > 2)
+    PyList_Append(list, Py_BuildValue("i", number));
+  return list;
 }
 
-static PyObject* method_generate_modulos(PyObject* self, PyObject* args) {
-    PyObject* factors = NULL;
-    int size = 0, max_iter = 0;
-    // parse argument "number"
-    // k= unsigned long long without overflow check
-    // O= PyObject*
-    if (!PyArg_ParseTuple(args, "Okk", &factors, &size, &max_iter)) {
-        return NULL;
-    }
-    PyObject* list = PyList_New(0);
-    uint64_t list_size = PyList_GET_SIZE(factors);
-    for (int i = 0; i < max_iter;i++) {
-        unsigned long long factor = PyLong_AsLong(PyList_GetItem(factors, rand() % list_size));
-        PyList_Append(list, Py_BuildValue("i", (i * factor) % size));
-    }
-    return list;
+static PyObject *method_generate_modulos(PyObject *self, PyObject *args) {
+  PyObject *factors = NULL;
+  luint size = 0, max_iter = 0;
+  // parse argument "number"
+  // K= unsigned long long
+  // O= PyObject*
+  if (!PyArg_ParseTuple(args, "OKK", &factors, &size, &max_iter)) {
+    return NULL;
+  }
+  PyObject *list = PyList_New(0);
+  uint64_t list_size = PyList_GET_SIZE(factors);
+  for (int i = 0; i < max_iter; i++) {
+    unsigned long long factor =
+        PyLong_AsLong(PyList_GetItem(factors, rand() % list_size));
+    PyList_Append(list, Py_BuildValue("i", (i * factor) % size));
+  }
+  return list;
 }
 
 static PyMethodDef Calculations[] = {
     {"factors", method_factors, METH_VARARGS, "Number calculations"},
-    {"generate_modulos", method_generate_modulos, METH_VARARGS, "Number calculations"},
-    {NULL, NULL, 0, NULL}
-};
+    {"generate_modulos", method_generate_modulos, METH_VARARGS,
+     "Number calculations"},
+    {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef calculations = {
-    PyModuleDef_HEAD_INIT,
-    "calculations",
-    "Number calculations",
-    -1,
+    PyModuleDef_HEAD_INIT, "calculations", "Number calculations", -1,
     Calculations,
 };
 
 PyMODINIT_FUNC PyInit_calculations(void) {
-    srand((unsigned int)time(0));
-    return PyModule_Create(&calculations);
+  srand((unsigned int)time(0));
+  return PyModule_Create(&calculations);
 }
